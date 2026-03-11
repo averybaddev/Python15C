7b3c3223c79bde5f1506255be5866fed27c7b1af
averybaddev <tabletappsmiles@gmail.com>
2026-03-11 15:23:49 +0100

    Add files via upload

--- Py15C.py
+++ Py15C.py
@@ -83,9 +83,12 @@ class MathOps:
         MathOps implements all math operations in the HP15C.
         It initializes some constants such as pi.
         All functions implemented are implemented in the order they are mentioned in the manual.
+        The comments such as NUMBER ALTERATION FUNCTIONS are titles directly from the manual.
         """
         self.pi = 3.141592653 #the HP manual states that the pi button places the first 10 digits of pi into the calculator.
-        
+    
+    
+    # NUMBER ALTERATION FUNCTIONS
     def integer(self):
         calc.stack.x = int(calc.stack.x)
         
@@ -98,6 +101,8 @@ class MathOps:
     def absolute_value(self): #cant name this abs since python already has a function named abs
         calc.stack.x = abs(calc.stack.x)
     
+    
+    # ONE NUMBER FUNCTIONS
     def reciprocal(self):
         calc.stack.x = 1/calc.stack.x
         
@@ -128,6 +133,85 @@ class MathOps:
         """
         calc.stack.x = calc.stack.x ** calc.stack.x
         
+        
+    # TRIGONOMETRY
+    def sine(self):
+        if calc.trig_mode==0: #degrees mode
+            calc.stack.x = math.sin(math.radians(calc.stack.x)) #converts the degrees to radians for the calculation
+        elif calc.trig_mode==1: #radians mode
+            calc.stack.x = math.sin(calc.stack.x)
+        else: #gradians mode
+            calc.stack.x = math.sin(calc.stack.x*self.pi/200) #the gradian to radian conversion (value*pi/200) was graciously provided by google
+        
+    def arcsine(self):
+        if calc.trig_mode==0:
+            calc.stack.x = math.asin(math.radians(calc.stack.x))
+        if calc.trig_mode==1:
+            calc.stack.x = math.asin(calc.stack.x)
+        if calc.trig_mode==2:
+            calc.stack.x = math.asin(calc.stack.x*self.pi/200)
+    
+    def cosine(self):
+        if calc.trig_mode==0:
+            calc.stack.x = math.cos(math.radians(calc.stack.x))
+        if calc.trig_mode==1:
+            calc.stack.x = math.cos(calc.stack.x)
+        if calc.trig_mode==2:
+            calc.stack.x = math.cos(calc.stack.x*self.pi/200)
+        
+    def arccosine(self):
+        if calc.trig_mode==0:
+            calc.stack.x = math.acos(math.radians(calc.stack.x))
+        if calc.trig_mode==1:
+            calc.stack.x = math.acos(calc.stack.x)
+        if calc.trig_mode==2:
+            calc.stack.x = math.acos(calc.stack.x*self.pi/200)
+            
+    def tangent(self):
+        if calc.trig_mode==0:
+            calc.stack.x = math.tan(math.radians(calc.stack.x))
+        if calc.trig_mode==1:
+            calc.stack.x = math.tan(calc.stack.x)
+        if calc.trig_mode==2:
+            calc.stack.x = math.tan(calc.stack.x*self.pi/200)
+        
+    def arctangent(self):
+        if calc.trig_mode==0:
+            calc.stack.x = math.atan(math.radians(calc.stack.x))
+        if calc.trig_mode==1:
+            calc.stack.x = math.atan(calc.stack.x)
+        if calc.trig_mode==2:
+            calc.stack.x = math.atan(calc.stack.x*self.pi/200)
+        
+    # TIME AND ANGLE CONVERSIONS
+    def to_hms(self): #real struggle to get working, floating points are so finnicky, which is the reason why there are so many rounds.
+        """
+        Converts a decimal hour value into Hours Minutes and Seconds.
+        The following format is used: h.mmssxxxxx
+        x is for the sub second values.
+        Since this function was a mess to write, I will explain the entirety of it with comments.
+        """
+        hours = int(calc.stack.x) #if the value is 1.5 decimal hours, it will grab just the 1.
+        the_rest = round(calc.stack.x - hours,10) #in this case, if the value is 1.5, it will do 1.5 - the number of hours, which we saw previously is 1, so 1.5-1 = 0.5. The round is there just because of python floating point nonsense.
+        if the_rest == 0: #if there are no decimals, and just the hour, it will set everything to 0. example: 3 decimal hours = 3.000000000 after the function runs. 
+            minutes, seconds, below_seconds = 0, 0, 0 #setting the variables to 0 (if they arent set, there will be an error when running the final line)
+        else: #however, if there are decimals in the number, so for example 1.5, it will proceed with the next steps of the conversion.
+            minutes  = round(the_rest*60,10) #it sets the number of minutes by taking the rest, and multiplying it by 60.
+            if round(minutes-int(minutes),10)!=0: #but its possible for the value to minutes to have a decimal. that means we have to continue further with the conversion
+                seconds = round((minutes - int(minutes))*60,10) #it sets the number of seconds by taking the fractional part of minutes, and multiplying it by 60
+                minutes = int(minutes) # this removes the fractional part of minutes since that fractional part has now been turned into seconds.
+                if round(seconds-int(seconds),10)!=0: #if the seconds have a fractional part, it will proceed for the conversion to subseconds
+                    below_seconds = round(seconds-int(seconds),10) #this will go down to the hundred thousandth of a second, as it is stated to be the maximum accuracy of this conversion in the HP15C manual.
+                else: #if there is no fractional part to seconds, it will simply set below_seconds to 0
+                    below_seconds = 0 #this is set to 0 to prevent errors with the last line.
+            else: #if the minutes dont have a fractional part, it will set seconds and below_seconds to 0
+                seconds, below_seconds = 0,0 #these are set to 0 to prevent errors with the last line
+        calc.stack.x = round(hours + minutes * 0.01 + seconds * 0.0001 + below_seconds * 0.000000001,9) #sets the value of x to the h.mmssxxxxx format.
+        
+    
+    def to_hh(self): #conversion to decimal hours
+        pass
+    
     def addition(self):
         """
         This function adds x and y together, with the output ending up in x.
@@ -162,6 +246,7 @@ class HP15C:
         self.display_digits = 5 #the hp15c has 5 digits displayed by default. cannot go to 0. #might need to be changed to digits after the period... come back later
         self.complex_numbers = False #if False, disable functionality entirely. same as flag 9 in the hp15c
         self.display_mode = 0 #0 for fixed (standard hp15c mode), 1 for scientific, 2 for engineering.
+        self.trig_mode = 0 #0 for degrees (decimal, not minutes seconds), 1 for radians, 2 for gradians.
 
         
     def change_display_digits(self,number):
@@ -178,8 +263,6 @@ class HP15C:
 
 #"automated" stack testing area
 calc = HP15C()
-calc.stack.add_element_to_stack(3)
-calc.stack.add_element_to_stack(5)
-calc.stack.show_stack_for_debug()
-calc.math.addition()
+calc.stack.add_element_to_stack(1.0166666666667)
+calc.math.to_hms()
 calc.stack.show_stack_for_debug()
